f7ced71a7d3b34021c144377a7044c1962750f5d
sean mackenzie <sean_mackenzie@ucsb.edu>
2023-09-17 16:11:17 -0700

    first commit

---
+++ examples/external/Keithley I-V Sweep SM zipper.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Keithley I-V Sweep
+Demis John, 2018-10-12
+
+Program to sweep voltage & measure current on Keithley SMU
+Known bugs: the plot window opens *behind* the current window.
+Also, file is saved *usually* in same directory as this script - but at one point it kept saving into the PythonXY directory, which was confusing.  Probably need to set the Python working directory to be sure.  Use `import os; os.getcwd()` to see where it will save.
+
+Edit/Run this file via Spyder / Anaconda Python
+Installed PyVISA for communication.
+Updated for Python 3.x
+
+
+Based off Steve Nichols' Script from ~2010
+"""
+
+##########################################
+##########################################
+''' User Settings:     '''
+
+SaveFiles = True   # Save the plot & data?
+DevName = 'w25 - Au on thermal SiO2' #in filename of  saved plot 
+
+NewFolder = True    # save data into a subfolder?
+FolderName = 'data'    # Only used if NewFolder=True
+
+Keithley_GPIB_Addr = 22     # GPIB Address of the Keithley (in Menu/Comms)
+
+'''Voltage Sweep settings'''
+CurrentCompliance = 1.00e-3    # compliance (max) current, in Amps
+intV = 0       # start and end voltage
+minV = -150      # min voltage
+maxV = 150      # max voltage
+numpoints = 50   # number of points in sweep
+dir = 'pos'      # initial sweep direction: 'pos'itive or 'neg'ative.
+
+##########################################
+##########################################
+
+
+''' You shouldn't need to edit anything below'''
+## Import some modules:
+import pyvisa as visa             # PyVISA module fro GPIB communication, installed
+import time             # to allow pause between measurements
+import os               # manipulate file paths and make directories
+import numpy as np      # matlab-like array math
+
+#from pylab import *    # for matlab-style plotting commands like `plot(x,y)`
+import matplotlib.pyplot as plt # for python-style plottting, like 'ax1.plot(x,y)'
+
+
+# organize Voltage array into sequence
+minVdesc = np.linspace(intV,minV,numpoints)
+minVasc  = minVdesc[::-1]
+maxVasc  = np.linspace(intV, maxV, numpoints)
+maxVdesc = maxVasc[::-1]
+
+if dir == 'pos':
+    Vspace   = np.concatenate((maxVasc,maxVdesc,minVdesc,minVasc), axis=0)
+elif dir == 'neg':
+    Vspace   = np.concatenate((minVdesc,minVasc,maxVasc,maxVdesc), axis=0)
+#if intV != 0:
+#    initVsweep = np.linspace(0,intV,numpoints)
+#    Vspace = np.concatenate((initVsweep, Vspace), axis=0)
+
+
+# Open Visa connections to instruments
+#keithley = visa.GpibInstrument(22)     # GPIB addr 22
+rm = visa.ResourceManager()
+keithley = rm.get_instrument(  'GPIB::' + str(Keithley_GPIB_Addr)  )
+
+
+# Setup Keithley for  current loop
+
+# Setup electrodes that are voltage
+keithley.write("*RST")
+#print("reset the instrument")
+time.sleep(0.5)    # add second between
+keithley.write(":SOUR:FUNC:MODE VOLT")
+keithley.write(":SENS:CURR:PROT:LEV " + str(CurrentCompliance))
+keithley.write(":SENS:CURR:RANGE:AUTO 1")   # set current reading range to auto (boolean)
+keithley.write(":OUTP ON")                    # Output on    
+
+Voltage=[]
+Current = []
+for V in Vspace:
+    #Voltage.append(V)
+    print("Voltage set to: " + str(V) + " V" )
+    keithley.write(":SOUR:VOLT " + str(V))
+    time.sleep(0.1)    # add second between
+    data = keithley.ask(":READ?")   #returns string with many values (V, I, ...)
+    answer = data.split(',')    # remove delimiters, return values into list elements
+    I = eval( answer.pop(1) ) * 1e6     # convert to number
+    Current.append( I )
+    
+    vread = eval( answer.pop(0) )
+    Voltage.append(vread)
+    #Current.append(  I  )          # read the current
+    
+    print("--> Current = " + str(Current[-1]) + ' uA')   # print last read value
+#end for(V)
+keithley.write(":OUTP OFF")
+
+#set to current source, voltage meas
+keithley.write(":SOUR:FUNC:MODE curr")
+keithley.write(":SOUR:CURR " + str(CurrentCompliance))
+keithley.write(":SENS:volt:PROT:LEV " + str(max(Voltage))  )
+keithley.write(":SENS:volt:RANGE:AUTO 1")
+
+keithley.write("SYSTEM:KEY 23") # go to local control
+keithley.close()
+    
+###### Plot #####
+    
+fig1, ax1 = plt.subplots(nrows=1, ncols=1)         # new figure & axis
+
+line1 = ax1.plot(Voltage[0:numpoints], Current[0:numpoints], 'b+-', label='ascending #1')
+line1 = ax1.plot(Voltage[numpoints-1:numpoints*2], Current[numpoints-1:numpoints*2],'.-',color='lightskyblue', label='descending #1')
+line1 = ax1.plot(Voltage[numpoints*2-1:numpoints*3], Current[numpoints*2-1:numpoints*3], 'r.-', label='ascending #2')
+line1 = ax1.plot(Voltage[numpoints*3-1:], Current[numpoints*3-1:], '+-', color='lightcoral', label='descending #2')
+
+ax1.set_xlabel('Voltage (V)')
+ax1.set_ylabel('Current (uA)')
+ax1.set_title('I-V Curve - ' + DevName)
+
+ax1.grid(True)
+plt.legend()
+
+fig1.show()  # draw & show the plot - unfortunately it often opens underneath other windows
+
+
+if SaveFiles:
+    curtime = time.strftime('%Y-%m-%d_%H%M.%S')
+    SavePath = 'I-V Curve - ' + DevName + ' - [' + curtime +']'
+    # create subfolder if needed:
+    if NewFolder and not os.path.isdir(FolderName): os.mkdir(FolderName)
+    if NewFolder: SavePath = os.path.join(FolderName, SavePath )
+    fig1.savefig(  SavePath + '.png'  )
+    
+    data = np.array(  list( zip(Current, Voltage) )  )
+    np.savetxt( SavePath + '.txt', data, fmt="%e", delimiter="\t", header="Current (mA)\tVoltage (V)" )
+    print("Saved data to:\n    %s" %(os.path.abspath(SavePath))  )
+    #np.array(Voltage).tofile(  os.path.join(DevName, 'I-V Voltage - ' + DevName + ' - [' + curtime +'].txt' )  )
+    #np.array(Current).tofile(  os.path.join(DevName, 'I-V Current - ' + DevName + ' - [' + curtime +'].txt' )  )
+#end if(SaveFiles)
+
+
+#attempt to force figure to front:
+fig1.canvas.window().raise_()
---
+++ examples/external/example_pyvisa.py
@@ -0,0 +1,93 @@
+import pyvisa
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# 1. A GENERAL EXAMPLE
+
+rm = pyvisa.ResourceManager()
+"""
+After importing pyvisa, we create a ResourceManager object. If called without arguments,
+PyVISA will prefer the default backend (IVI) which tries to find the VISA shared library 
+for you. If it fails it will fall back to pyvisa-py if installed. You can check what backend 
+is used and the location of the shared library used, if relevant, simply by:
+
+print(rm)
+# returns somethign like: <ResourceManager('/path/to/visa.so')>
+
+"""
+
+rm.list_resources()
+# returns something like: ('ASRL1::INSTR', 'ASRL2::INSTR', 'GPIB0::14::INSTR')
+my_instrument = rm.open_resource('GPIB0::14::INSTR')
+"""
+Once that you have a ResourceManager, you can list the available resources using the list_resources method. 
+The output is a tuple listing the VISA resource names. You can use a dedicated regular expression syntax to 
+filter the instruments discovered by this method. The syntax is described in details in list_resources(). 
+The default value is ‘?*::INSTR’ which means that by default only instrument whose resource name ends 
+with ‘::INSTR’ are listed (in particular USB RAW resources and TCPIP SOCKET resources are not listed). 
+To list all resources present, pass ‘?*’ to list_resources.
+
+In this case, there is a GPIB instrument with instrument number 14, so you ask the ResourceManager to 
+open “‘GPIB0::14::INSTR’” and assign the returned object to the my_instrument.
+
+Notice open_resource has given you an instance of GPIBInstrument class (a subclass of the more generic Resource).
+"""
+
+print(my_instrument.query('*IDN?'))
+"""
+Then, you query the device with the following message: '\*IDN?'. Which is the standard GPIB message for 
+“what are you?” or – in some cases – “what’s on your display at the moment?”. query is a short form for 
+a write operation to send a message, followed by a read.
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# 2. A KEITHLEY EXAMPLE
+
+rm = pyvisa.ResourceManager()
+keithley = rm.open_resource("GPIB::12")
+keithley.write("*rst; status:preset; *cls")
+"""
+Above, we create the instrument variable keithley, which is used for all further operations on the instrument. 
+Immediately after it, we send the initialization and reset message to the instrument.
+"""
+
+interval_in_ms = 500
+number_of_readings = 10
+keithley.write("status:measurement:enable 512; *sre 1")
+keithley.write("sample:count %d" % number_of_readings)
+keithley.write("trigger:source bus")
+keithley.write("trigger:delay %f" % (interval_in_ms / 1000.0))
+keithley.write("trace:points %d" % number_of_readings)
+keithley.write("trace:feed sense1; trace:feed:control next")
+"""
+The above step is to write all the measurement parameters, in particular the interval time (500ms) and 
+the number of readings (10) to the instrument. I won’t explain it in detail. Have a look at an SCPI and/or 
+Keithley 2000 manual.
+"""
+
+keithley.write("initiate")
+keithley.assert_trigger()
+keithley.wait_for_srq()
+"""
+Okay, now the instrument is prepared to do the measurement. The above three lines make the instrument wait 
+for a trigger pulse, trigger it, and wait until it sends a “service request”:
+"""
+
+keithley.write("initiate")
+keithley.assert_trigger()
+keithley.wait_for_srq()
+"""
+By sending the service request, the instrument tells us that the measurement has been finished and that 
+the results are ready for transmission. 
+"""
+
+voltages = keithley.query_ascii_values("trace:data?")
+print("Average voltage: ", sum(voltages) / len(voltages))
+""" read data from Keithley """
+
+keithley.query("status:measurement?")
+keithley.write("trace:clear; trace:feed:control next")
+"""
+Finally, we should reset the instrument’s data buffer and SRQ status register, so that it’s ready for a new run.
+"""
