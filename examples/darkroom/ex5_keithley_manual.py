684cfa298b0ff02e4a8924e3be6ad970267b1af1
sean mackenzie <sean_mackenzie@ucsb.edu>
2023-09-17 23:44:18 -0700

    added some examples from Keithley 2410 User Manual

---
+++ examples/darkroom/ex5_keithley_manual.py
@@ -0,0 +1,43 @@
+import pyvisa
+import numpy as np
+
+GPIB = 23
+
+source_voltage_range = 20  # Volts
+source_voltage_level = 10  # Volts
+sense_current_protection = 1e-3  # Amps
+sense_current_range = 1e-3  # Amps
+
+# ---
+
+rm = pyvisa.ResourceManager()
+keithley = rm.open_resource("GPIB::{}".format(GPIB))
+
+# 0.
+keithley.write('*RST')  # Restore GPIB default
+
+# 1. Select source function, mode
+keithley.write(':SOUR:FUNC VOLT')  # Select voltage source.
+keithley.write(':SOUR:VOLT:MODE FIXED')  # Fixed voltage source mode.
+
+# 2. Set source range, level, compliance
+keithley.write('SOUR:VOLT:RANG ' + str(source_voltage_range))  # Select 20V source range.
+keithley.write(':SOUR:VOLT:LEV ' + str(source_voltage_level))  # Source output = 10V.
+keithley.write(':SENS:CURR:PROT ' + str(sense_current_protection))  # 1mA compliance.
+
+# 3. Set measure function, range
+keithley.write(':SENS:FUNC "CURR"')  # Current measure function.
+keithley.write(':SENS:CURR:RANG ' + str(sense_current_range))  # 1mA measure range.
+keithley.write(':FORM:ELEM CURR')  # Current reading only.
+
+# 4. Turn on output
+keithley.write('OUTP ON')  # Output on before measuring.
+
+# 5. Read data
+keithley.query_ascii_values(':READ?', container=np.array)  # Trigger, acquire reading.
+# NOTE: Instrument must be addressed to talk after :READ? to acquire data.
+
+# 6. Turn off output
+keithley.write(':OUTP OFF')  # End example given in manual
+
+# I assume you would want to shut down the device after this.
