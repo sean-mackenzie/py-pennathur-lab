3c18897cf8bfc285f722546b6b74760d5d97ee94
Sean MacKenzie <sean_mackenzie@ucsb.edu>
2025-02-11 21:13:42 -0800

    tightened up arbitrary waveform source voltage and monitoring code.

--- test/test_Agilent_33210A.py
+++ test/test_Agilent_33210A.py
@@ -7,29 +7,84 @@ import pyvisa
 import time
 
 
-def setup_6517_amplifier_monitor(keithley_inst, settings):
+def package_data_and_export(data, data_elements, settings, return_df):
+    df = pd.DataFrame(data, columns=data_elements.split(','))
+    df_settings = pd.DataFrame.from_dict(data=settings, orient='index')
+
+    file = join(settings['save_dir'], '{}_data.xlsx'.format(settings['save_id']))
+    sns, dfs = ['data', 'settings'], [df, df_settings]
+    idx, lbls = [False, True], [None, 'k']
+    with pd.ExcelWriter(file) as writer:
+        for sheet_name, dataframe, idx, idx_lbl in zip(sns, dfs, idx, lbls):
+            dataframe.to_excel(writer, sheet_name=sheet_name, index=idx, index_label=idx_lbl)
+    if return_df:
+        return df
+
+def plot_arbitrary_waveform_monitor_and_monitor(df, settings):
+    # setup
+    # df.columns = ['READ', 'TST']
+    py, px = df.columns
+
+    # sampling rate
+    dt = df[px].iloc[-1]
+    num_samples = len(df)
+    samples_per_second = np.round(num_samples / dt, 2)
+
+    # plot
+    fig, ax1 = plt.subplots()#nrows=3, figsize=(10, 10))
+    ax1.plot(df[px], df[py], '-o', ms=2, label=samples_per_second)
+    ax1.set_xlabel('{} (s)'.format(px))
+    ax1.set_ylabel(settings['keithley_measure_units'])
+    ax1.grid(alpha=0.2)
+    ax1.legend(title='#/s', loc='upper right', fontsize='small')
+    plt.suptitle(settings['save_id'])
+    plt.tight_layout()
+    plt.savefig(join(settings['save_dir'], 'fig_{}.png'.format(settings['save_id'])),
+                dpi=300, facecolor='w', bbox_inches='tight')
+    plt.show()
+    plt.close()
+
+def post_process_data(data, data_elements, settings):
+    df = package_data_and_export(data, data_elements, settings, return_df=True)
+    plot_arbitrary_waveform_monitor_and_monitor(df=df, settings=settings)
+
+
+
+def setup_keithley_6517_amplifier_monitor(keithley_inst, settings):
     """
     Hardware configuration for unguarded voltage measurement:
         1. The HI (red) from triax cable should be connected for high voltage side (i.e., amplifier output).
         2. The LO (black) from triax cable should be connected for low voltage side (i.e., ground).
     :param keithley_inst:
-    :param nplc:
+    :param settings:
     :return:
     """
+    # hard-coded Keithley modifiers
+    ratio_fetch_to_integration = 2
     # -
-    k1_integration_period = k1_nplc / 60
-    k1_delay_time = k1_integration_period * 2
-    estimated_timeout = k1_num_samples * k1_integration_period * 1000  # (ms)
-    print("estimate timeout: {} seconds".format(estimated_timeout / 1000))
-    k1.timeout = estimated_timeout  # Set the timeout error time (units: ms) for PyVISA
-    if k1_monitor == 'CURR':
-        k1_measure_units = '1V/40mA'
-    elif k1_monitor == 'VOLT':
-        k1_measure_units = '1V/100V'
+    integration_period = settings['keithley_nplc'] / 60
+    fetch_delay = integration_period * ratio_fetch_to_integration
+    estimated_timeout = settings['keithley_num_samples'] * integration_period  # (seconds)
+    print("estimated timeout: {} seconds".format(estimated_timeout))
+    if settings['keithley_monitor'] == 'CURR':
+        measure_units = '1V/40mA'
+    elif settings['keithley_monitor'] == 'VOLT':
+        measure_units = '1V/100V'
     else:
         raise ValueError("Invalid monitor type.")
+    keithley_settings = {
+        'keithley_measure_units': measure_units,
+        'keithley_integration_period': integration_period,
+        'keithley_fetch_delay': fetch_delay,
+        'keithley_ratio_fetch_to_integration': ratio_fetch_to_integration,
+        'keithley_timeout': estimated_timeout,
+    }
+    settings.update(keithley_settings)
     # -
     # --- INITIALIZE KEITHLEY 6517a/b
+    # -
+    # Set instrument timeout
+    keithley_inst.timeout = estimated_timeout * 1000  # Set the timeout error time (units: ms) for PyVISA
     # -
     # 1. RESET to defaults
     keithley_inst.write('*RST')
@@ -53,7 +108,7 @@ def setup_6517_amplifier_monitor(keithley_inst, settings):
     keithley_inst.write(':ARM:LAYer2:DEL 0')  # After receiving Arm Layer 2 Event, delay before going to Trigger Layer
     # -
     keithley_inst.write(':TRIG:TCON:DIR ACC')  # Wait for trigger event (TLINK)
-    keithley_inst.write(':TRIG:COUN ' + str(num_samples))  # Set measure count (1 to 99999 or INF)
+    keithley_inst.write(':TRIG:COUN ' + str(settings['keithley_num_samples']))  # Set measure count (1 to 99999 or INF)
     keithley_inst.write(':TRIG:SOUR IMM')  # Select control source (HOLD, IMMediate, TIMer, MANual, BUS, TLINk, EXTernal) (default: IMM)
     keithley_inst.write(':TRIG:DEL 0')  # After receiving Measure Event, delay before Device Action
 
@@ -66,7 +121,7 @@ def setup_6517_amplifier_monitor(keithley_inst, settings):
     keithley_inst.write(':SENS:FUNC "VOLT"')  # 'VOLTage[:DC]', 'CURRent[:DC]', 'RESistance', 'CHARge' (default='VOLT:DC')
     keithley_inst.write(':SENS:VOLT:DC:GUARd OFF')  # Disable guard
     # k3.write(':SENS:CURR:APERture <n>')       # (default: 60 Hz = 16.67 ms) Set integration rate in seconds: 167e-6 to 200e-3
-    keithley_inst.write(':SENS:VOLT:NPLC ' + str(nplc))  # (default = 1) Set integration rate in line cycles (0.01 to 10)
+    keithley_inst.write(':SENS:VOLT:NPLC ' + str(settings['keithley_nplc']))  # (default = 1) Set integration rate in line cycles (0.01 to 10)
     # --- perform zero correct
     # see page 248 of manual for instructions
     # --- end zero correct
@@ -80,198 +135,170 @@ def setup_6517_amplifier_monitor(keithley_inst, settings):
     keithley_inst.write(':SYST:ZCOR OFF')  # Enable (ON) or disable (OFF) zero correct (default: OFF)
     keithley_inst.write(':SYST:ZCH OFF')  # Enable (ON) or disable (OFF) zero check (default: OFF)
 
+    return settings
+
+def required_input_to_amplifier(gain, output_voltage, dc_offset):
+    req_input_volt = np.round(output_voltage / gain, 3)
+    req_input_dc_offset = np.round(dc_offset / gain, 3)
+    return req_input_volt, req_input_dc_offset
 
 def setup_agilent_awg(agilent_inst, settings):
-    dict_settings = {
-        'save_id': save_id,
-        'awg_wave': awg_wave,
-        'awg_freq': awg_freq,
-        'awg_square_duty_cycle': awg_square_duty_cycle,
-        'awg_volt_unit': awg_volt_unit,
-        'Vapp': Vapp,
-        'Voffset': Voffset,
-        'trek_ampli': trek_ampli,
-        # 'awg_volt': awg_volt,
-        # 'awg_volt_offset': awg_volt_offset,
-        'awg_output_termination': awg_output_termination,
-        'k1_monitor': k1_monitor,
-        'k1_nplc': k1_nplc,
-        'k1_num_samples': k1_num_samples,
-        # 'k1_integration_period': k1_integration_period,
-        # 'k1_delay_time': k1_delay_time,
-        # 'k1_measure_units': k1_measure_units,
-        # 'estimated_timeout': estimated_timeout,
-    }
-
-    awg_volt, awg_volt_offset = np.round(Vapp / trek_ampli, 3), np.round(Voffset / trek_ampli, 2)
     # -
-    if awg_volt + awg_volt_offset > 18:
+    if settings['awg_volt'] + settings['awg_dc_offset'] > 18:
         raise ValueError("AWG max Vpp + Voffset is 20.")
-    elif awg_volt > 18:
+    elif settings['awg_volt'] > 18:
         raise ValueError("AWG max Vpp is 20.")
     else:
-        print("AWG Voltage Output: {} {} + {} DC".format(awg_volt, awg_volt_unit, awg_volt_offset))
-        print("TREK Voltage Output: {} {} + {} DC".format(Vapp, awg_volt_unit, Voffset))
+        print("AWG Voltage Output: {} {} + {} DC".format(settings['awg_volt'], settings['awg_volt_unit'], settings['awg_dc_offset']))
+        print("TREK Voltage Output: {} {} + {} DC".format(settings['output_volt'], settings['awg_volt_unit'], settings['output_dc_offset']))
 
     awg.write('*RST')  # Restore GPIB default
     # Program physical/hardware set up
-    awg.write('OUTP:LOAD ' + str(awg_output_termination))  # OUTPut:LOAD {<ohms>|INFinity|MINimum|MAXimum}
+    awg.write('OUTP:LOAD ' + str(settings['awg_output_termination']))  # OUTPut:LOAD {<ohms>|INFinity|MINimum|MAXimum}
     # NOTE 1: the input impedance of the Trek Amplifier is 90 kOhms.
     # NOTE 2: it seems the "most appropriate" output termination for Trek is 10 kOhms.
 
     # Program waveform
-    awg.write('FUNC ' + awg_wave)  # FUNCtion {SINusoid|SQUare|RAMP|PULSe|NOISe|DC|USER}
-    awg.write('FREQ ' + str(awg_freq))  # FREQuency {<frequency>|MINimum|MAXimum}
-    awg.write('VOLT ' + str(awg_volt))  # VOLTage {<amplitude>|MINimum|MAXimum}
-    awg.write('VOLT:OFFS ' + str(awg_volt_offset))  # VOLTage:OFFSet {<offset>|MINimum|MAXimum}
-    awg.write('VOLT:UNIT ' + awg_volt_unit)  # VOLTage:UNIT {VPP|VRMS|DBM}
+    awg.write('FUNC ' + settings['awg_wave'])  # FUNCtion {SINusoid|SQUare|RAMP|PULSe|NOISe|DC|USER}
+    awg.write('FREQ ' + str(settings['awg_freq']))  # FREQuency {<frequency>|MINimum|MAXimum}
+    awg.write('VOLT ' + str(settings['awg_volt']))  # VOLTage {<amplitude>|MINimum|MAXimum}
+    awg.write('VOLT:OFFS ' + str(settings['awg_dc_offset']))  # VOLTage:OFFSet {<offset>|MINimum|MAXimum}
+    awg.write('VOLT:UNIT ' + settings['awg_volt_unit'])  # VOLTage:UNIT {VPP|VRMS|DBM}
     awg.write('VOLT:RANG:AUTO ON')  # VOLTage:RANGe:AUTO {OFF|ON|ONCE}
     if awg_wave == 'SQU':
-        awg.write('FUNC:SQU:DCYC ' + str(awg_square_duty_cycle))  # FUNCtion:SQUare:DCYCle {<percent>|MINimum|MAXimum}
+        awg.write('FUNC:SQU:DCYC ' + str(settings['awg_square_duty_cycle']))  # FUNCtion:SQUare:DCYCle {<percent>|MINimum|MAXimum}
+
+
+def data_acquisition_handler(agilent_inst, keithley_inst, settings):
+    # 1. Trigger the Keithley to start recording data
+    keithley_inst.write(':SYST:TST:REL:RES')  # Reset relative timestamp to 0.
+    keithley_inst.write(':INIT')  # Trigger voltage readings.
+
+    # 2. Start sourcing voltage from arbitrary waveform
+    agilent_inst.write('OUTP ON')  # OUTPut {OFF|ON}
+
+    # 3. Handle periodic data acquisition
+    data = []
+    for i in range(settings['keithley_num_samples']):
+        time.sleep(settings['keithley_fetch_delay'])
+        datum = k1.query_ascii_values(':FETCh?', container=np.array)  # request data.
+        data.append(datum)
+
+    # 4. Stop sourcing voltage
+    awg.write('OUTP OFF')
+
+    # 5. Return data and elements
+    data = np.array(data)
+    data_elements = keithley_inst.query(':FORMat:ELEM?').rstrip()
+
+    return data, data_elements
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
-    # --- SETUP
+    # --- HARDWARE SETUP
+    # available instruments
     rm = pyvisa.ResourceManager()
     check_inst = False  # True False
     if check_inst is True:
         print(rm.list_resources())  # only list "::INSTR" resources
-        # # returns something like: ('ASRL1::INSTR', 'ASRL2::INSTR', 'GPIB0::14::INSTR')
         raise ValueError("Check instruments are connected.")
-
-    # --- PROGRAMMING
-    # AWG_GPIB, AWG_BOARD_INDEX = 10, 0  # Arbitrary waveform generator (awg)
-    AWG_USB = 'USB0::0x0957::0x1507::MY48003320::INSTR'  # None
-    K1_GPIB, K1_BOARD_INDEX = 24, 0  # Arbitrary waveform generator (awg)
+    # instrument addresses
+    # Agilent 33210A aribtrary waveform generator
+    AWG_USB = 'USB0::0x0957::0x1507::MY48003320::INSTR'  # or, AWG_GPIB, AWG_BOARD_INDEX = 10, 0
     AMPLIFIER_GAIN = 50
     AWG_OUTPUT_TERMINATION = '10E3'
+    # Keithley 6517 electrometer
+    K1_GPIB, K1_BOARD_INDEX = 24, 0
 
     # ------------------------------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
-    # Setup
-    SAVE_DIR = r'C:\Users\nanolab\Desktop\sean\Agilent-Keithley-Andor Synchronization\Test-Current-Monitor'
+    # --- TEST SETUP
 
+    # root directory
+    BASE_DIR = r'C:\Users\nanolab\Desktop\sean\Agilent-Keithley-Andor Synchronization'
+
+    # test id
+    TEST_SUBJECT = 'Test-Current-Monitor'
     TID = 6
-    # -
+
     # Agilent 33210A arbitrary waveform generator
     AWG_WAVE = 'SIN'  # SIN, SQU, RAMP, PULS, DC
     AWG_FREQ = 0.25  # 0.001 to 10000000
+    OUTPUT_VOLT = 200  # max bipolar: 350 V; max unipolar: 700 V
+    OUTPUT_DC_OFFSET = 0  # max: 350 V
     AWG_SQUARE_DUTY_CYCLE = 50  # 20 to 80 (square waves only)
     AWG_VOLT_UNIT = 'VPP'  # VPP, VRMS
-    TARGET_VOLT, VOFFSET = 200, 0
-    # -
-    # Keithley 6517a monitor
-    k1_monitor = 'CURR'  # 'CURR' or 'VOLT
-    k1_nplc, k1_num_samples = 1, 250
-    # -
-    # save id
-    save_id = ('tid{}_{}_{}Hz_{}pDC_{}{}_{}VDCoffset'.format(tid, awg_wave, awg_freq,
-                                                             awg_square_duty_cycle,
-                                                             Vapp, awg_volt_unit, Voffset))
 
-    if tid is not None:
-        dict_settings = {
-            'save_id': save_id,
-            'awg_wave': awg_wave,
-            'awg_freq': awg_freq,
-            'awg_square_duty_cycle': awg_square_duty_cycle,
-            'awg_volt_unit': awg_volt_unit,
-            'Vapp': Vapp,
-            'Voffset': Voffset,
-            'trek_ampli': trek_ampli,
-            #'awg_volt': awg_volt,
-            #'awg_volt_offset': awg_volt_offset,
-            'awg_output_termination': awg_output_termination,
-            'k1_monitor': k1_monitor,
-            'k1_nplc': k1_nplc,
-            'k1_num_samples': k1_num_samples,
-            #'k1_integration_period': k1_integration_period,
-            #'k1_delay_time': k1_delay_time,
-            #'k1_measure_units': k1_measure_units,
-            #'estimated_timeout': estimated_timeout,
+    # Keithley 6517a monitor
+    K1_MONITOR = 'CURR'  # 'CURR' or 'VOLT
+    K1_NPLC = 1  # 0.01 to 10
+    K1_NUM_SAMPLES = 250
+
+    # ---
+
+    # setup directories and filenames for export
+    SAVE_ID = ('tid{}_{}_{}Hz_{}pDC_{}{}_{}VDC-offset'.format(
+        TID, AWG_WAVE, AWG_FREQ, AWG_SQUARE_DUTY_CYCLE, OUTPUT_VOLT, AWG_VOLT_UNIT, OUTPUT_DC_OFFSET))
+    SAVE_DIR = join(BASE_DIR, TEST_SUBJECT)
+    if not os.path.exists(SAVE_DIR):
+        os.makedirs(SAVE_DIR)
+
+    if TID is not None:
+        AWG_VOLT, AWG_DC_OFFSET = required_input_to_amplifier(
+            gain=AMPLIFIER_GAIN,
+            output_voltage=OUTPUT_VOLT,
+            dc_offset=OUTPUT_DC_OFFSET,
+        )
+        DICT_SETTINGS = {
+            'tid': TID,
+            'save_dir': SAVE_DIR,
+            'save_id': SAVE_ID,
+            'output_volt': OUTPUT_VOLT,
+            'output_dc_offset': OUTPUT_DC_OFFSET,
+            'awg_wave': AWG_WAVE,
+            'awg_freq': AWG_FREQ,
+            'awg_square_duty_cycle': AWG_SQUARE_DUTY_CYCLE,
+            'awg_volt_unit': AWG_VOLT_UNIT,
+            'awg_volt': AWG_VOLT,
+            'awg_dc_offset': AWG_DC_OFFSET,
+            'awg_output_termination': AWG_OUTPUT_TERMINATION,
+            'amplifier_gain': AMPLIFIER_GAIN,
+            'keithley_monitor': K1_MONITOR,
+            'keithley_nplc': K1_NPLC,
+            'keithley_num_samples': K1_NUM_SAMPLES,
         }
+    else:
+        raise ValueError("Must define tid.")
 
     # ------------------------------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
     # --- Initialize instruments
-    # Agilent 33210A arbitrary waveform generator
-    awg = rm.open_resource(awg_usb)  # 'GPIB{}::{}::INSTR'.format(awg_board_index, awg_GPIB)
-    # Keithley 6517a electrometer
-    k1 = rm.open_resource('GPIB{}::{}::INSTR'.format(k1_board_index, k1_GPIB))
-
+    AWG = rm.open_resource(AWG_USB)  # 'GPIB{}::{}::INSTR'.format(awg_board_index, awg_GPIB)
+    K1 = rm.open_resource('GPIB{}::{}::INSTR'.format(K1_BOARD_INDEX, k1_GPIB))
     # -
-
     # --- Program instruments
-    dict_settings = setup_agilent_awg(agilent_inst=awg, settings=dict_settings)
-    dict_settings = setup_6517_amplifier_monitor(keithley_inst=k1, settings=dict_settings)
-
-    # -----------------
-
-
-
-    awg.write('OUTP ON')  # OUTPut {OFF|ON}
-
-
-    # Trigger the monitor Keithley
-    k1.write(':SYST:TST:REL:RES')  # Reset relative timestamp to 0.
-    k1.write(':INIT')  # Trigger voltage readings.
-    data_monitor = []
-    for i in range(k1_num_samples):
-        time.sleep(k1_delay_time)
-        datum = k1.query_ascii_values(':FETCh?', container=np.array)  # request data.
-        data_monitor.append(datum)
-        # print("{}: {}".format(i, datum))
-
-    time.sleep(1)
-
-    awg.write('OUTP OFF')
-
-    # post-process stimulus data
+    DICT_SETTINGS = setup_agilent_awg(agilent_inst=AWG, settings=DICT_SETTINGS)
+    DICT_SETTINGS = setup_keithley_6517_amplifier_monitor(keithley_inst=K1, settings=DICT_SETTINGS)
     # -
-    # --- parse, package, and export
-    data_elements = k1.query(':FORMat:ELEM?').rstrip()
-    print(data_elements)
-    # df = parse_ascii(d=data, e=data_elements, as_type='pd.DataFrame')
-    data_monitor = np.array(data_monitor)
-    df = pd.DataFrame(data_monitor, columns=data_elements.split(','))
-    df_settings = pd.DataFrame.from_dict(data=dict_settings, orient='index')
+    # --- Acquire data
+    DATA, DATA_ELEMENTS = data_acquisition_handler(
+        agilent_inst=AWG,
+        keithley_inst=K1,
+        settings=DICT_SETTINGS,
+    )
+    # -
+    # - Post-process data and save
+    post_process_data(
+        data=DATA,
+        data_elements=DATA_ELEMENTS,
+        settings=DICT_SETTINGS,
+    )
 
-    file = join(save_dir, '{}_data.xlsx'.format(save_id))
-    sns, dfs = ['data', 'settings'], [df, df_settings]
-    idx, lbls = [False, True], [None, 'k']
-    with pd.ExcelWriter(file) as writer:
-        for sheet_name, dataframe, idx, idx_lbl in zip(sns, dfs, idx, lbls):
-            dataframe.to_excel(writer, sheet_name=sheet_name, index=idx, index_label=idx_lbl)
 
-    # --- PLOTTING
-
-    # setup
-    # df.columns = ['READ', 'TST']
-    print(df.columns)
-    py, px = df.columns
-
-    dt = df[px].iloc[-1]
-    num_samples = len(df)
-    samples_per_second = np.round(num_samples / dt, 2)
-    print("{} samples / {} s = {} samples/s".format(num_samples, dt, samples_per_second))
-
-    fig, ax1 = plt.subplots()#nrows=3, figsize=(10, 10))
-
-    ax1.plot(df[px], df[py], '-o', ms=2, label=samples_per_second)
-    ax1.set_xlabel('{} (s)'.format(px))
-    ax1.set_ylabel(k1_measure_units)
-    ax1.grid(alpha=0.2)
-    ax1.legend(title='#/s', loc='upper right', fontsize='small')
-
-    plt.suptitle(save_id)
-    plt.tight_layout()
-    plt.savefig(join(save_dir, 'fig_{}.png'.format(save_id)), dpi=300, facecolor='w', bbox_inches='tight')
-    plt.show()
-    plt.close()
 
     print("Completed without errors.")
